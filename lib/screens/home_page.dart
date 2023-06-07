d40ea115001073ae61957b5ad5414c51ddc08f67
Amirhossein Zeinali <amirhossein.zeinali22@gmail.com>
2023-06-07 10:37:56 +0330

    LibraryPage implemented

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -80,7 +80,38 @@ class _HomePageState extends State<HomePage> {
                         ],
                       ),
                     ),
-                    Container()
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        children: [
+                          DropdownButton<String>(
+                            // Step 3.
+                            value: dropDownValue,
+                            items: <String>[
+                              'علاقه مندی ها',
+                              'پر فروش ها',
+                              'جدید ترین ها'
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              );
+                            }).toList(),
+                            // Step 5.
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropDownValue = newValue!;
+                              });
+                            },
+                          ),
+                          SizedBox(height: 15),
+                          generateAudioBooks(dropDownValue),
+                        ],
+                      ),
+                    ),
                   ],
                 ))
               ],
@@ -91,7 +122,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  generateBooks(String dropDownValue) {
+  generateAudioBooks(String dropDownValue) {
     return Scrollbar(
       child: SizedBox(
         height: 450,
@@ -101,8 +132,38 @@ class _HomePageState extends State<HomePage> {
           children: List.generate(
             6,
             (index) => GestureDetector(
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => DetailsPage()));
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => DetailsPage()));
+              },
+              child: Container(
+                height: 100,
+                child: Column(
+                  children: [
+                    Flexible(child: Image.asset("assets/images/sample.jpg")),
+                    Text("$dropDownValue AudioBook $index")
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  generateBooks(String dropDownValue) {
+    return Scrollbar(
+      child: SizedBox(
+        height: 450,
+        child: GridView.count(
+          crossAxisCount: 2,
+          scrollDirection: Axis.vertical,
+          children: List.generate(
+            6,
+                (index) => GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => DetailsPage()));
               },
               child: Container(
                 height: 100,
--- lib/screens/library_page.dart
+++ lib/screens/library_page.dart
@@ -8,8 +8,70 @@ class LibraryPage extends StatefulWidget {
 }
 
 class _LibraryPageState extends State<LibraryPage> {
+  String dropDownValue = 'کتاب';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: 15,)
+          ,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                DropdownButton<String>(
+                  value: dropDownValue,
+                  items: <String>[
+                    'تاریخ مطالعه',
+                    'فایل صوتی',
+                    'کتاب' ,
+                    'بیشترین پسند'
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(fontSize: 13),
+                      ),
+                    );
+                  }).toList(),
+                  // Step 5.
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropDownValue = newValue!;
+                    });
+                  },
+                ),
+                const Text("مرتب سازی بر اساس"),
+              ],
+            ),
+          ),
+          SizedBox(height: 15,),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.75,
+            width: MediaQuery.of(context).size.width,
+            child: Scrollbar(
+              child: GridView.count(
+                crossAxisCount: 3,
+                children: List.generate(
+                  10,
+                  (index) => Container(
+                    width: 100,
+                    child: Column(
+                      children: [
+                        Flexible(child: Image.asset("assets/images/sample.jpg")),
+                        Text("Book Name index:$index")
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
