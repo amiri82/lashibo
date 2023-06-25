d9651cb5cedd3a29499343a09fe848377c010d0c
Amirhossein Zeinali <amirhossein.zeinali22@gmail.com>
2023-06-25 23:36:39 +0330

    Added Validator To Register Page and Added two additional packages

--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -19,8 +20,19 @@ class _RegisterPageState extends State<RegisterPage> {
     fontSize: 15,
   );
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final illegalCharacters = [
+    FilteringTextInputFormatter.deny(RegExp(r"\s"), replacementString: "")
+  ];
+
   @override
   Widget build(BuildContext context) {
+    _emailController.text = "amirhossein.zeinali22@gmail.com"; //TODO : remove this line
+    _passwordController.text = "Amiri1382"; //TODO : remove this line
+    _usernameController.text = "amir"; //TODO : remove this line
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -35,71 +47,120 @@ class _RegisterPageState extends State<RegisterPage> {
               child: Localizations.override(
                 context: context,
                 locale: const Locale("fa", "IR"),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.account_circle,
-                      size: 85,
-                    ),
-                    const SizedBox(
-                      height: 8.0,
-                    ),
-                    const Text(
-                      "ثبت نام با ایمیل",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.account_circle,
+                        size: 85,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    SizedBox(
-                      height: 50,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          suffixIcon: const Icon(Icons.account_circle),
-                          labelText: "نام کاربری",
-                          labelStyle: fieldStyle,
-                          floatingLabelBehavior: FloatingLabelBehavior.auto,
-                          floatingLabelStyle: floatingLabelStyle,
+                      const SizedBox(
+                        height: 8.0,
+                      ),
+                      const Text(
+                        "ثبت نام با ایمیل",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    SizedBox(
-                      height: 50,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(10),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          minHeight: 50,
+                          maxHeight: 90,
+                        ),
+                        child: TextFormField(
+                          controller: _usernameController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "نام کاربری نمی تواند خالی باشد";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            suffixIcon: const Icon(Icons.account_circle),
+                            labelText: "نام کاربری",
+                            labelStyle: fieldStyle,
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                            floatingLabelStyle: floatingLabelStyle,
                           ),
-                          suffixIcon: const Icon(Icons.email),
-                          labelText: "ایمیل",
-                          labelStyle: fieldStyle,
-                          floatingLabelBehavior: FloatingLabelBehavior.auto,
-                          floatingLabelStyle: floatingLabelStyle,
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30.0,
-                    ),
-                    SizedBox(
-                      height: 50,
-                      child: TextField(
-                        obscureText: showPassword,
-                        decoration: InputDecoration(
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          minHeight: 50,
+                          maxHeight: 90,
+                        ),
+                        child: TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _emailController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "ایمیل نمیتواند خالی باشد!";
+                            } else {
+                              RegExp emailReg =
+                                  RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+                              if (!emailReg.hasMatch(value)) {
+                                return "لظفا یک ایمیل معتبر وارد کنید!";
+                              }
+                              return null;
+                            }
+                          },
+                          inputFormatters: illegalCharacters,
+                          decoration: InputDecoration(
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            suffixIcon: const Icon(Icons.email),
+                            labelText: "ایمیل",
+                            labelStyle: fieldStyle,
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                            floatingLabelStyle: floatingLabelStyle,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          minHeight: 50,
+                          maxHeight: 100,
+                        ),
+                        child: TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: _passwordController,
+                          obscureText: !showPassword,
+                          inputFormatters: illegalCharacters,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "رمزعبور نمی تواند خالی باشد";
+                            } else {
+                              RegExp passReg = RegExp(
+                                  r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+                              if (!passReg.hasMatch(value)) {
+                                return "رمزعبور باید حداقل ۸ کاراکتر و شامل حداقل یک حرف بزرگ و یک عدد باشد";
+                              }
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            errorMaxLines: 2,
                             filled: true,
                             labelText: "رمز عبور",
                             labelStyle: fieldStyle,
@@ -110,36 +171,43 @@ class _RegisterPageState extends State<RegisterPage> {
                             floatingLabelStyle: floatingLabelStyle,
                             floatingLabelBehavior: FloatingLabelBehavior.auto,
                             suffixIcon: GestureDetector(
-                              child: showPassword
+                              child: !showPassword
                                   ? const Icon(Icons.visibility_off)
                                   : const Icon(Icons.visibility),
                               onTap: () {
-                                setState(() {
-                                  showPassword = !showPassword;
-                                });
+                                setState(
+                                  () {
+                                    showPassword = !showPassword;
+                                  },
+                                );
                               },
-                            )),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(13),
-                            )),
-                        child: const Text("ثبت نام"),
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("ثبت نام با موفقیت انجام شد")));
+                              Navigator.of(context).pop();
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(13),
+                              )),
+                          child: const Text("ثبت نام"),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
